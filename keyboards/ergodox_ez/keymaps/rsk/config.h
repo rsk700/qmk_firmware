7281460d2b0dd219c8964626515a0be098e9cb6a
rsk <rskbox@gmail.com>
2016-07-19 01:38:10 +0300

    mouse tweaks

---
+++ keyboards/ergodox_ez/keymaps/rsk/config.h
@@ -0,0 +1,21 @@
+#ifndef CONFIG_RSK_H
+#define CONFIG_RSK_H
+
+#include "../../config.h"
+
+#undef MOUSEKEY_TIME_TO_MAX
+#define MOUSEKEY_TIME_TO_MAX 20
+
+#undef MOUSEKEY_MAX_SPEED
+#define MOUSEKEY_MAX_SPEED 6
+
+#undef MOUSEKEY_MOVE_DELTA
+#define MOUSEKEY_MOVE_DELTA 1
+
+#undef MOUSEKEY_INTERVAL
+#define MOUSEKEY_INTERVAL 12
+
+#undef MOUSEKEY_DELAY
+#define MOUSEKEY_DELAY 0
+
+#endif
